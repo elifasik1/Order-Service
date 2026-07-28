fb7f1e531da3eadfc4c55c00091c0457eb8114fd
elifasik1 <132699726+elifasik1@users.noreply.github.com>
2026-07-28 16:09:09 +0300

    refactor: replace in-memory repository with EF Core

--- src/OrderService.Infrastructure/Repositories/OrderRepository.cs
+++ src/OrderService.Infrastructure/Repositories/OrderRepository.cs
@@ -1,19 +1,28 @@
+using System.Security.Cryptography;
 using Domain.Entities;
 using OrderService.Application.Interfaces;
+using OrderService.Infrastructure.Data;
+
 namespace OrderService.Infrastructure.Repositories;
 
 
 
 public class OrderRepository : IOrderRepository
 {
-    private List<Order> _orders = new();
+    private readonly ApplicationDbContext _context;
+
+    public OrderRepository(ApplicationDbContext context)
+    {
+         _context = context;
+    }
     public void Add(Order order)
     {
-        _orders.Add(order);
+        _context.Orders.Add(order);
+        _context.SaveChanges();
     }
 
     public List<Order> GetAll()
     {
-        return _orders;
+        return _context.Orders.ToList();
     }
 }
