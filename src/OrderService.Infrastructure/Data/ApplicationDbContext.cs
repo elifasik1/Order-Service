8eb4aef2369bb0b1a69a17450bd348f579c14bd2
elifasik1 <132699726+elifasik1@users.noreply.github.com>
2026-08-13 22:50:41 +0300

    feat: add seed admin user

--- src/OrderService.Infrastructure/Data/ApplicationDbContext.cs
+++ src/OrderService.Infrastructure/Data/ApplicationDbContext.cs
@@ -11,13 +11,27 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
     public DbSet<RefreshToken> RefreshTokens => Set<RefreshToken>();
 
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
+ protected override void OnModelCreating(ModelBuilder modelBuilder)
 {
     base.OnModelCreating(modelBuilder);
 
     modelBuilder.Entity<Order>()
         .Property(x => x.Version)
         .IsRowVersion();
+
+ modelBuilder.Entity<User>().HasData(
+    new User
+    {
+        Id = Guid.Parse("11111111-1111-1111-1111-111111111111"),
+        FirstName = "System",
+        LastName = "Admin",
+        Email = "admin@orderservice.com",
+        PasswordHash = "Admin123!",
+        UserRole = Domain.Enums.UserRole.Admin,
+        UserStatus = Domain.Enums.UserStatus.Active,
+        CreatedAt = new DateTime(2026, 1, 1, 0, 0, 0, DateTimeKind.Utc)
+    }
+);
 }
     public override async Task<int> SaveChangesAsync(
         CancellationToken cancellationToken = default)
---
+++ src/OrderService.Infrastructure/Migrations/20260813194439_SeedAdminUser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OrderService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedAdminUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "CreatedAt", "Email", "FirstName", "LastName", "PasswordHash", "UserRole", "UserStatus" },
+                values: new object[] { new Guid("11111111-1111-1111-1111-111111111111"), new DateTime(2026, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc), "admin@orderservice.com", "System", "Admin", "Admin123!", 0, 0 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("11111111-1111-1111-1111-111111111111"));
+        }
+    }
+}
